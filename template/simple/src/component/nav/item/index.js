c138c15198176073ceedddbd63b5a8f2c2777d1c
zhangpan <zhangpan@icsoc.net>
2020-03-19 08:42:25 +0800

    【新增】simple模板sass 导航 prettier格式化调整

---
+++ template/simple/src/component/nav/item/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import classnames from 'classnames';
+import { Link } from 'react-router-dom';
+import './index.scss';
+
+export default class NavItem extends React.PureComponent {
+  onClick = () => {
+    const { order, onClick } = this.props;
+    if (onClick) {
+      onClick(order);
+    }
+  };
+
+  render() {
+    const {
+      className,
+      pathname,
+      isLeaf,
+      nav: { title },
+      index,
+    } = this.props;
+
+    const Com = isLeaf ? Link : 'div';
+
+    return (
+      <Com
+        className={classnames('nav-item', className, {
+          ['nav-item-leaf']: isLeaf,
+          ['nav-item-notleaf']: !isLeaf,
+        })}
+        to={pathname}
+        title={title}
+        onClick={this.onClick}>
+        {`${index.join('.')} ${title}`}
+      </Com>
+    );
+  }
+}
--- template/simple/webpack.dll.js
+++ template/simple/webpack.dll.js
@@ -1,24 +1,24 @@
-const webpack = require("webpack");
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require('webpack');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const pkg = require("./package.json");
+const pkg = require('./package.json');
 
 module.exports = {
-  mode: "none",
+  mode: 'none',
   entry: {
-    vendor: Object.keys(pkg.dependencies)
+    vendor: Object.keys(pkg.dependencies),
   },
   output: {
-    path: path.resolve(__dirname, "./dll"),
-    filename: "[name].dll.js",
-    library: "[name]"
+    path: path.resolve(__dirname, './dll'),
+    filename: '[name].dll.js',
+    library: '[name]',
   },
   plugins: [
     // new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: ["dll"] }),
     new webpack.DllPlugin({
-      path: path.resolve(__dirname, "./dll/[name]-mainfest.json"),
-      name: "[name]"
-    })
-  ]
+      path: path.resolve(__dirname, './dll/[name]-mainfest.json'),
+      name: '[name]',
+    }),
+  ],
 };
